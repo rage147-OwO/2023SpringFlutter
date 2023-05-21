595f11dd4ebb05d2b5b8c82c5c1e2bfcb5ec8d54
rage147-OwO <bbbaa1004@gmail.com>
2023-05-21 21:16:20 +0900

    .

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,24 +1,13 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:file_picker/file_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:path_provider/path_provider.dart';
+import 'model_manager.dart';
+import 'flutterObjectDetection/RunModelByCamera.dart';
 
 void main() {
   runApp(ModelManagementApp());
-}
-
-class Model {
-  final String name;
-  final List<String> classes;
-  final List<int> inputSize;
-  final int nc;
-  final String description;
-  String filePath;
-
-  Model(this.name, this.classes, this.inputSize, this.description, this.filePath)
-      : nc = classes.length;
 }
 
 class ModelManagementApp extends StatelessWidget {
@@ -40,19 +29,32 @@ class ModelListScreen extends StatefulWidget {
 }
 
 class _ModelListScreenState extends State<ModelListScreen> {
-  List<Model> models = [
-    Model('Model 1', ['Class A', 'Class B'], [224, 224],
-        'This is Model 1, it supports image input size of 224x224.',
-        '/path/to/model1.pt'),
-    Model('Model 2', ['Class X', 'Class Y', 'Class Z'], [512, 512],
-        'Model 2 is a powerful model with image input size of 512x512.',
-        '/path/to/model2.pt'),
-  ];
+  List<Model> models = [];
+  final ModelManager modelManager = ModelManager();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadModels();
+  }
+
+  void _loadModels() async {
+    models = await modelManager.loadModels();
+    if (models == null) {
+      models = [];
+    }
+    setState(() {});
+  }
+
+  void _saveModels() async {
+    await modelManager.saveModels(models);
+  }
 
   void _deleteModel(int index) {
     setState(() {
       models.removeAt(index);
     });
+    _saveModels();
   }
 
   void _addNewModelDialog() {
@@ -100,7 +102,14 @@ class _ModelListScreenState extends State<ModelListScreen> {
                 ),
                 ElevatedButton(
                   onPressed: () async {
-                    filePath = (await openAndCopyFile())!;
+                    FilePickerResult? result = await FilePicker.platform
+                        .pickFiles();
+                    Directory appDocDir = await getApplicationDocumentsDirectory();
+                    File file = File(result!.files.single.path!);
+                    String newPath = '${appDocDir.path}/${result.files.single
+                        .name}';
+                    File copiedFile = await file.copy(newPath);
+                    filePath = copiedFile.path;
                   },
                   child: Text("Select model(.pt) file"),
                 ),
@@ -115,12 +124,13 @@ class _ModelListScreenState extends State<ModelListScreen> {
               child: Text('Cancel'),
             ),
             TextButton(
-              onPressed: () {
+              onPressed: () async {
                 Model newModel = Model(
                     name, classes, inputSize, description, filePath);
                 setState(() {
                   models.add(newModel);
                 });
+                _saveModels();
                 Navigator.pop(context);
               },
               child: Text('Add'),
@@ -134,138 +144,68 @@ class _ModelListScreenState extends State<ModelListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Model List'),
-      ),
-      body: ListView.builder(
-        itemCount: models.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(models[index].name),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppBar(
+          title: Text('Model List'),
+        ),
+        body: ListView.builder(
+          itemCount: models.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(models[index].name),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Classes: ${models[index].classes.join(', ')}'),
+                  Text(
+                      'Input Size: ${models[index].inputSize[0]}x${models[index]
+                          .inputSize[1]}'),
+                  Text('Class Count: ${models[index].nc}'),
+                  Text('Description: ${models[index].description}'),
+                  Text('File Path: ${models[index].filePath}'),
+                ],
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  _deleteModel(index);
+                },
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RunModelByCameraDemo(models[index]),
+                  ),
+                );
+              },
+
+            );
+          },
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text('Classes: ${models[index].classes.join(', ')}'),
-                Text('Input Size: ${models[index].inputSize[0]}x${models[index].inputSize[1]}'),
-                Text('Class Count: ${models[index].nc}'),
-                Text('Description: ${models[index].description}'),
-                Text('File Path: ${models[index].filePath}'),
+                FloatingActionButton(
+                  onPressed: () {
+                    modelManager.printAllModelFiles();
+                  },
+                  child: Icon(Icons.print),
+                ),
+                SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: () {
+                    _addNewModelDialog();
+                    },
+                  child: Icon(Icons.add),
+                ),
               ],
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                _deleteModel(index);
-              },
-            ),
-            onTap: () {
-              // 항목 선택 시 동작
-            },
-          );
-        },
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              _addNewModelDialog();
-            },
-            child: Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              print(getAppFolderFiles());
-            },
-            child: Icon(Icons.folder_open_sharp),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          ],
+        )
     );
   }
-
-  Future<List<String>> getAppFolderFiles() async {
-    Directory appDir = await getApplicationDocumentsDirectory();
-    List<FileSystemEntity> entities = appDir.listSync();
-
-    List<String> fileNames = [];
-    for (FileSystemEntity entity in entities) {
-      if (entity is File) {
-        String filePath = entity.path;
-        fileNames.add(filePath);
-        print(filePath);
-      }
-    }
-
-    return fileNames;
-  }
-
-
-
-  Future<String?> openAndCopyFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-
-    File file = File(result!.files.single.path!);
-    String newPath = '${appDocDir.path}/${result.files.single.name}';
-    File copiedFile = await file.copy(newPath);
-
-
-    return copiedFile.path;
-  }
-
 }
-
-
-
-Future<File> copyFile(File sourceFile, String newPath) async {
-  try {
-    // 파일을 복제하기 전에 존재하는지 확인합니다.
-    if (await sourceFile.exists()) {
-      // 목적지 경로에 동일한 파일 이름이 있는 경우를 처리하기 위해 파일 이름을 추출합니다.
-      String fileName = sourceFile.path.split('/').last;
-
-      // 복제할 파일의 경로를 생성합니다.
-      String copiedFilePath = '$newPath/$fileName';
-
-      // 파일을 복제합니다.
-      await sourceFile.copy(copiedFilePath);
-
-      // 복제된 파일을 반환합니다.
-      return File(copiedFilePath);
-    } else {
-      throw FileSystemException('Source file does not exist.');
-    }
-  } catch (e) {
-    // 복제 과정에서 발생한 오류를 처리합니다.
-    print('Error copying file: $e');
-    throw e;
-  }
-}
-
-
-
-Future<void> saveTextToFile(String text) async {
-  Directory appDocDir = await getApplicationDocumentsDirectory();
-  String filePath = '${appDocDir.path}/data.txt';
-
-  File file = File(filePath);
-  await file.writeAsString(text);
-}
-
-Future<String?> readTextFromFile() async {
-  Directory appDocDir = await getApplicationDocumentsDirectory();
-  String filePath = '${appDocDir.path}/data.txt';
-
-  File file = File(filePath);
-  if (await file.exists()) {
-    String text = await file.readAsString();
-    return text;
-  }
-
-  return null;
-}
-
-
-
--- ui/lib/model_manager.dart
+++ ui/lib/model_manager.dart
@@ -1,59 +1,93 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+class Model {
+  final String name;
+  final List<String> classes;
+  final List<int> inputSize;
+  final int nc;
+  final String description;
+  String filePath;
+
+  Model(this.name, this.classes, this.inputSize, this.description, this.filePath)
+      : nc = classes.length;
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'classes': classes,
+    'inputSize': inputSize,
+    'nc': nc,
+    'description': description,
+    'filePath': filePath,
+  };
+
+  factory Model.fromJson(Map<String, dynamic> json) {
+    return Model(
+      json['name'],
+      List<String>.from(json['classes']),
+      List<int>.from(json['inputSize']),
+      json['description'],
+      json['filePath'],
+    );
+  }
+}
 
 class ModelManager {
-  static Future<List<String>> getAppFolderFiles() async {
-    Directory appDir = await getApplicationDocumentsDirectory();
-    List<FileSystemEntity> entities = appDir.listSync();
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
 
-    List<String> fileNames = [];
-    for (FileSystemEntity entity in entities) {
-      if (entity is File) {
-        String filePath = entity.path;
-        fileNames.add(filePath);
-        print(filePath);
-      }
+  Future<File> get _modelListJsonFile async {
+    final path = await _localPath;
+    return File('$path/models.json');
+  }
+
+  Future<void> saveModels(List<Model> models) async {
+    final file = await _modelListJsonFile;
+    final jsonModels = models.map((model) => model.toJson()).toList();
+    final jsonString = json.encode(jsonModels);
+
+    // Create the file if it doesn't exist
+    if (!(await file.exists())) {
+      await file.create(recursive: true);
     }
 
-    return fileNames;
+    await file.writeAsString(jsonString);
   }
 
-  static Future<String?> openAndCopyFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    if (result != null) {
-      Directory appDocDir = await getApplicationDocumentsDirectory();
-
-      String fileName = result.files.single.name;
-      String newPath = '${appDocDir.path}/$fileName';
-
-      File file = File(result.files.single.path!);
-      File copiedFile = await file.copy(newPath);
-
-      return copiedFile.path;
-    } else {
-      return null;
+  Future<List<Model>> loadModels() async {
+    try {
+      final file = await _modelListJsonFile;
+      final jsonString = await file.readAsString();
+      final jsonModels = json.decode(jsonString) as List;
+      return jsonModels.map((json) => Model.fromJson(json)).toList();
+    } catch (e) {
+      // If encountering an error, return an empty list
+      return [];
     }
   }
 
-  static Future<void> saveTextToFile(String text) async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String filePath = '${appDocDir.path}/data.txt';
-
-    File file = File(filePath);
-    await file.writeAsString(text);
-  }
-
-  static Future<String?> readTextFromFile() async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String filePath = '${appDocDir.path}/data.txt';
-
-    File file = File(filePath);
+  void deleteModelFile() async {
+    final file = File('models.json');
     if (await file.exists()) {
-      String text = await file.readAsString();
-      return text;
+      await file.delete();
     }
+  }
+  Future<void> printAllModelFiles() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final files = directory.listSync();
 
-    return null;
+      for (var file in files) {
+        if (file is File) {
+          print(path.basename(file.path));
+        }
+      }
+    } catch (e) {
+      print('Error printing model files: $e');
+    }
   }
 }
